2d1692dce188f1eda49e6800240d3afb6e25f78f
RayJair22 <44067242+RayJair22@users.noreply.github.com>
2018-10-18 11:03:14 -0500

    HOLA DANI

--- Calculator/Calculator/Controllers/CalcController.cs
+++ Calculator/Calculator/Controllers/CalcController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Calculator.Models;
 
 namespace Calculator.Controllers
 {
@@ -13,5 +14,64 @@ namespace Calculator.Controllers
         {
             return View();
         }
+
+        public ActionResult index(int res)
+        {
+            int val1 = int.Parse(Request.Form["val1"].ToString());
+            int val2 = int.Parse(Request.Form["val2"].ToString());
+            string oper = Request.Form["oper"].ToString();
+            Calculadora calc = new Calculadora();
+            switch (oper)
+            {
+                case "+":
+                    try {
+                        calc.suma(val1, val2);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("No es un formato soportado");
+                    }
+                    break;
+
+                case "-":
+                    try
+                    {
+                        calc.resta(val1, val2);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("No es un formato soportado");
+                    }
+                    break;
+                    break;
+
+                case "*":
+                    try
+                    {
+                        calc.mult(val1, val2);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("No es un formato soportado");
+                    }
+                    break;
+                    break;
+
+                case "/":
+                    try
+                    {
+                        calc.d(val1, val2);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("No es un formato soportado");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("No es un operador valido");
+                    break;
+            }
+            return View(res);
+        }
     }
-}
+    }
