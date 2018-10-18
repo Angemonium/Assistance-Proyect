afc7f0fb762154cee7c999ac101213434f73778a
Angemonium <44067283+Angemonium@users.noreply.github.com>
2018-10-18 10:16:11 -0500

    se agrego el controlador

---
+++ Calculator/Calculator/Controllers/CalcController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
