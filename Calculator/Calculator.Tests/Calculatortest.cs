8f7650a906b99a9458a7ba581f7a696b0600b1e2
RayJair22 <44067242+RayJair22@users.noreply.github.com>
2018-10-18 12:11:28 -0500

    CalculatorTest

---
+++ Calculator/Calculator.Tests/Calculatortest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Models;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    class Calculatortest
+    {
+        [TestMethod]
+        public void Testsuma()
+        {
+            var s = new Calculadora();
+            var result = s.suma(5, 3);
+            Assert.AreEqual(8, result);
+        }
+    }
+}
